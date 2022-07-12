d54ad91c4781da50a0a10cb4ccad9b300207cf90
JoaaoJ <raposacomtuberculose@gmail.com>
2022-07-12 17:42:31 -0300

    First Componnent

--- fronted/src/App.tsx
+++ fronted/src/App.tsx
@@ -1,5 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-return(<h1>Olá mundo!</h1>)
+    return(
+    <>
+      <h1>Site muito bom</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+    )
 }
 
-export default App
+    export default App
